cb62c6a529ead4d4ddb2a52a02819ada1e661169
Zahid Kizmaz <tech@zahid.rocks>
2023-10-28 13:13:22 +0200

    feat: update post_gen_hook to support dir deletion for .github folder

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,25 +1,33 @@
 import pathlib
 
 REMOVE_PATHS = [
+    "{% if cookiecutter.use_github_actions == false %}.github{% endif %}",
     "{% if cookiecutter.use_tool_versions == false %}.tool-versions{% endif %}",
 ]
 
 
 def main():
-    rm()
+    rm_unused_files_and_dirs(REMOVE_PATHS)
 
 
-def rm():
+def rm_unused_files_and_dirs(paths: list[str]) -> None:
     project_path = pathlib.Path(".")
-    for path_str in REMOVE_PATHS:
+    for path_str in paths:
         path_str = path_str.strip()
-        path = project_path / pathlib.Path(path_str)
+        if not path_str:
+            continue
 
-        if path_str and path.exists():
-            if path.is_dir():
-                path.rmdir()
-            else:
-                path.unlink()
+        path = project_path / pathlib.Path(path_str)
+        remove_dir_recursively(path)
+
+
+def remove_dir_recursively(path: pathlib.Path) -> None:
+    if path.is_dir():
+        for child in path.iterdir():
+            remove_dir_recursively(child)
+        path.rmdir()
+    else:
+        path.unlink()
 
 
 if __name__ == "__main__":
